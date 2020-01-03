de5e347e74bce7a504a81691e7609e1fefa55d21
Omar Mirza <omar.creative.dev@gmail.com>
2020-01-03 16:06:54 +0000

    updated header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import { faEnvelope, faPhone, faGlobe, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public faEnvelope: IconDefinition = faEnvelope;
+  public faPhone: IconDefinition = faPhone;
+  public faGlobe: IconDefinition = faGlobe;
+  public faMapMarkerAlt: IconDefinition = faMapMarkerAlt;
+}
