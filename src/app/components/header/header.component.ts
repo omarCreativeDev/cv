96c4ffb5b00418fd246e909f1f5cd1b9c8c3eabb
Omar Mirza <omar.creative.dev@gmail.com>
2020-01-05 07:44:02 +0000

    updated header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IconDefinition } from '@fortawesome/fontawesome-common-types';
-import { faEnvelope, faPhone, faGlobe, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhoneAlt, faGlobe, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,35 @@ import { faEnvelope, faPhone, faGlobe, faMapMarkerAlt } from '@fortawesome/free-
 })
 export class HeaderComponent {
   public faEnvelope: IconDefinition = faEnvelope;
-  public faPhone: IconDefinition = faPhone;
+  public faPhoneAlt: IconDefinition = faPhoneAlt;
   public faGlobe: IconDefinition = faGlobe;
   public faMapMarkerAlt: IconDefinition = faMapMarkerAlt;
+  public faLinkedinIn: IconDefinition = faLinkedinIn;
+  public headerList: any[] = [
+    {
+      href: 'tel:07932 566 488',
+      icon: this.faPhoneAlt,
+      label: '07932 566 488',
+    },
+    {
+      href: 'mailto:omar.creative.dev@gmail.com?subject=Creative%20Developments',
+      icon: this.faEnvelope,
+      label: 'omar.creative.dev@gmail.com',
+    },
+    {
+      href: 'https://www.linkedin.com/in/omarCreativeDev/',
+      icon: this.faLinkedinIn,
+      label: 'linkedin.com/in/omarCreativeDev',
+    },
+    {
+      href: 'https://tinyurl.com/tepxxtd',
+      icon: this.faMapMarkerAlt,
+      label: 'Barkingside, London',
+    },
+    {
+      href: 'http://www.creativedevelopments.net',
+      icon: this.faGlobe,
+      label: 'creativedevelopments.net',
+    },
+  ];
 }
